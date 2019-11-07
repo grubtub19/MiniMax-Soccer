8dbad14a866cadda487f1ca28bd354a644613f48
grubtub19 <probe1919@gmail.com>
2019-11-06 18:40:37 -0600

    first commit

---
+++ src/projects/proj2/evaluation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import math, random
+from ...lib.game import discrete_soccer, connect_four
+
+def soccer(state, player_id):
+    # TODO: Implement this function!
+    #
+    # The soccer evaluation function *must* look into the game state
+    # when running. It will then return a number, where the larger the
+    # number, the better the expected reward (or lower bound reward)
+    # will be.
+    #
+    # For a good evaluation function, you will need to
+    # SoccerState-specific information. The file
+    # `src/lib/game/discrete_soccer.py` provides a description of all
+    # useful SoccerState properties.
+    if not isinstance(state, discrete_soccer.SoccerState):
+        raise ValueError("Evaluation function incompatible with game type.")
+    return 0
+
+def connect_four(state, player_id):
+    if not isinstance(state, connect_four.Connect4State):
+        raise ValueError("Evaluation function incompatible with game type.")
+    return 0
